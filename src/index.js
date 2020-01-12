4c579f3b6b4a5efc202d88075faf33888e1ef3ee
Noah Gambee <noah@gambee.dev>
2020-01-12 16:30:59 -0700

    Add dom update function calls after login validation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+// import classes
+import Customer from './Customer.js';
+import Manager from './Manager.js';
+import Bookings from './Bookings.js';
 
 let userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(response => response.json()).then(data => data.users);
 let roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then(response => response.json()).then(data => data.rooms);
 let bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(response => response.json()).then(data => data.bookings);
+let currentCustomer;
+let currentCustId = 0;
+let booking;
+let manager;
 
+Promise.all([userData, roomData, bookingData]).then(data => {
+  userData = data[0];
+  roomData = data[1];
+  bookingData = data[2];
+}).then(() => {
+
+})
 
 // EVENT LISTENERS //
 
