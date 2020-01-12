ecc42f8ca5ba518e663a16910b937694b09af05a
Noah Gambee <noah@gambee.dev>
2020-01-12 15:25:51 -0700

    Add username/password validation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+// EVENT LISTENERS //
+
+$('.login-btn').on('click', evaluateCredentials);
+
+// DOM UPDATES
+function evaluateCredentials() {
+  let passwordInputVal = $('.password-input').val();
+  let usernameInputVal = $('.username-input').val();
+  let customerUserName = usernameInputVal.slice(0,8);
+  let customerId = parseInt(usernameInputVal.slice(8,10));
+
+  if (usernameInputVal.length === 10 && customerUserName === 'customer' && customerId <= 50 && passwordInputVal === 'overlook2019') {
+    console.log('display customer dashboard');
+  } else if (usernameInputVal === 'manager' && passwordInputVal === 'overlook2019') {
+    console.log('display manager dashboard');
+  } else {
+    console.log('display log in error message');
+  }
+}
